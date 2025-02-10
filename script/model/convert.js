00fd6fdb5a8c33cc29bcb7b163f44e2e1c3babc4
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-10 07:51:17 +0700

    Add method to convert celcius to kelvin and fahren

--- script/model/convert.js
+++ script/model/convert.js
@@ -2,6 +2,17 @@ function Convert(tempInput, fromInput, toInput) {
   this.temperature = tempInput;
   this.from = fromInput;
   this.to = toInput;
+  this.result;
 }
+
+Convert.prototype.celcToKel = function (input) {
+  return input + 273.15;
+};
+
+Convert.prototype.celcToFah = function (input) {
+  let result = input * (9 / 5) + 32;
+  let roundedResult = parseFloat(result.toFixed(2));
+  return roundedResult;
+};
 
 export { Convert };
