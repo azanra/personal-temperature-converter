ab50753e21a8cab002e61dc4be2fd2148ec831f3
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-12 07:06:31 +0700

    Add function to create multiple element

--- script/util/util.js
+++ script/util/util.js
@@ -1,3 +1,5 @@
+import { createElement } from "../view/createElement.js";
+
 function roundNum(input) {
   let roundedInput = parseFloat(input.toFixed(2));
   return roundedInput;
@@ -6,6 +8,12 @@ function roundNum(input) {
 function setMultipleAttribute(element, attribute) {
   for (let key in attribute) {
     element.setAttribute(key, attribute[key]);
+  }
+}
+
+function createMultipleElement(amount, parent, element, attribute, text) {
+  for (let i = 0; i < amount; i++) {
+    createElement(parent, element, attribute, text);
   }
 }
 
