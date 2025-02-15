ce183ad5615108527673768be7523992273c4f29
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-15 10:56:38 +0700

    Add click listener to convert temperature input

---
+++ script/controller/convertController.js
@@ -0,0 +1,38 @@
+import { Convert } from "../model/convert.js";
+
+function ConvertController(arr) {
+  const convertBtn = document.querySelector(".convert-btn");
+  convertBtn.addEventListener("click", () => {
+    let fromSelect = document.querySelector("#from-select").value;
+    let toSelect = document.querySelector("#to-select").value;
+    let tempInput = document.querySelector("#input-temp").value;
+    tempInput = Number(tempInput);
+    let convertEntry = new Convert(tempInput, fromSelect, toSelect);
+    convertEntry = convertTemp(convertEntry);
+    return arr.push(convertEntry);
+  });
+}
+
+function convertTemp(convertObj) {
+  if (convertObj.from === "celcius" && convertObj.to === "kelvin") {
+    convertObj.result = convertObj.celcToKel();
+    return convertObj;
+  } else if (convertObj.from === "celcius" && convertObj.to === "fahrenheit") {
+    convertObj.result = convertObj.celcToFah();
+    return convertObj;
+  } else if (convertObj.from === "kelvin" && convertObj.to === "celcius") {
+    convertObj.result = convertObj.kelToCel();
+    return convertObj;
+  } else if (convertObj.from === "kelvin" && convertObj.to === "fahrenheit") {
+    convertObj.result = convertObj.kelToFah();
+    return convertObj;
+  } else if (convertObj.from === "fahrenheit" && convertObj.to === "celcius") {
+    convertObj.result = convertObj.fahToCel();
+    return convertObj;
+  } else if (convertObj.from === "fahrenheit" && convertObj.to === "kelvin") {
+    convertObj.result = convertObj.fahToKel();
+    return convertObj;
+  }
+}
+
+export { ConvertController };
